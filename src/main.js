d76e72a863dce8ea16e7d8e8ef1f546602ea4f7a
rafaeldemeterko <rafaeldemeterko@gmail.com>
2026-06-11 19:49:06 -0300

    feat: implement online resume site

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
 import './styles.css';
 
-const cards = document.querySelectorAll('.status-grid article');
-
-cards.forEach((card, index) => {
-  card.style.setProperty('--entry-delay', `${index * 80}ms`);
-});
+document.documentElement.dataset.ready = 'true';
